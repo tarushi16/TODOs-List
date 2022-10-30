9744016d34ce2cf5e191eeaeb2f7fedc3f375e57
tarushi16 <107392283+tarushi16@users.noreply.github.com>
2022-10-30 09:20:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,221 @@
+//ways to print in javascript
+//console.log("hello world");
+// alert("me");
+// document.write("this is document write")
+//javascript console API
+//   console.log("hello world", 4+6, "another log");
+//   console.warn("this is warning");
+//   console.error("this is an error");
+
+//variables
+var number1 = 34;
+var number2 = 56;
+//   console.log(number1 + number2);
+
+//data types
+//number
+var num1 = 455;
+var num2 = 56.76;
+//string
+var str1 = "this is a string";
+var str2 = "this is also a string";
+//object
+var marks = {
+  ravi: 34,
+  shubham: 78,
+  harry: 99.977,
+};
+//   console.log(num1)
+//   console.log(num2)
+//   console.log(str2)
+//   console.log(str1)
+//   console.log(marks)
+
+//booleans
+var a = true;
+var b = false;
+//   console.log(a,b)
+//   console.log(b)
+
+// var und;
+//   console.log(und);
+
+var n = null;
+//   console.log(n)
+
+//arrays
+var arr = [1, 2, "string", 4, 5];
+//   console.log(arr[2])
+//   console.log(arr)
+
+// operators
+// var p=34;
+// var q=56;
+// console.log("the value of p+q is ", p+q);
+// console.log("the value of p+q is ", p-q);
+// console.log("the value of p+q is ", p*q);
+// console.log("the value of p+q is ", p/q);
+
+// var c = q;
+// c += 2
+// console.log(c)
+
+var x = 34;
+var y = 56;
+// console.log(x==y)
+// console.log(x>=y)
+// console.log(x<=y)
+// console.log(x<y)
+// console.log(x>y)
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && false)
+// console.log(false && true)
+
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || false)
+// console.log(false || true)
+
+// console.log(!false);
+// console.log(!true);
+
+// function avg(a,b){
+//     return (a+b)/2;
+// }
+
+// c=avg(4,6);
+// console.log(c);
+// console.log('3')
+
+// var age = 4;
+// // if (age>8){
+// //     console.log('you are not a kid');
+// // }
+// // else{
+// //     console.log('you are a kid')
+// // }
+// age=31;
+// if (age>32){
+//     console.log("no")
+// }
+// else if(age>26){
+//     console.log("yes")
+// }
+// else if(age>22){
+//     console.log("hola")
+// }
+// else if(age>18){
+//     console.log("y")
+// }
+// else{
+//     console.log("lol")
+// }
+// console.log("end of lader")
+
+// var arr = [1,2,3,4,5,6,7];
+// // // console.log(arr);
+// for(var i = 0;i<arr.length;i++){
+//     if(i==5){
+//         // break
+//         continue;
+//     }  
+// console.log(arr[i])}
+
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+
+
+// const ac = 0;
+// ac = ac + 1;
+let j = 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+// do{
+//     console.log(arr[j]);
+//     j++;
+// }while(j<arr.length);
+
+// let  myArr = ["fan","camera",34 ,null, true];
+// console.log(myArr.length)
+// // myArr.pop();
+// // myArr.push("harry")
+// const newLen = myArr.unshift("harry")
+// // myArr.unshift("harry")
+// console.log(newLen)
+// console.log(myArr)
+// d.sort arr.sort
+
+// string menthods in javascript
+// let mylovelystring ="tarushi is a good girl"
+// console.log(mylovelystring.length)
+// console.log(mylovelystring.lastIndexOf("good"))
+
+// console.log(mylovelystring.slice(1,4))
+// myDate = new Date();
+// console.log(myDate.getHours());
+
+// let elem = document.getElementById('click');
+// console.log(elem);
+
+// let elemClass = document.getElementsByClassName("container")
+// console.log(elemClass);
+// // elemClass[0].style.background = "yellow";
+// elemClass[0].classList.add("bg-primary")
+
+// tn = document.getElementsByTagName('button')
+// console.log(tn)
+// createdElement = document.createElement('p');
+// createdElement.innerText = "this is a created para"
+// tn[0].appendChild(createdElement);
+
+// function clicked(){}
+// firstContainer.addEventListener('click me this is a created para',function(){
+//   console.log("click me this is a created para")
+// })
+
+// let prevHTML = document.querySelectorAll('.container')[1].innerHTML;
+// firstContainer.addEventListener('mouseup',function(){
+//   console.log("mouse up when clicked on container");
+// })
+
+// firstContainer.addEventListener('mousedown',function(){
+//   document.querySelectorAll('.container')[1].innerHTML = <b>We have clicked </b>
+//   console.group("mouse down when clicked on container");
+
+// })
+
+// function sum(a,b){
+//   return a+b;
+// }
+// sum = (a,b)=>{
+//   return a+b;
+// }
+// // console.log(a)
+// logkaro = ()=>{
+//   document.querySelectorAll('.container')[1].innerHTML = "<B> we have clicked</B>"
+//   console.log("i am your log")
+// }
+
+// clr = setInterval(logkaro, 2000);
+
+//JSON
+// obj = {name:"taru",length:1, a:{this:'th"at'}}
+// jso = JSON.stringify(obj);
+// console.log(jso)
+// parsed = JSON.parse(`{"name":"taru","length":1,"a":{"this":"that"}}`)
+// console.log(parsed);
+// localStorage.clear()
+
+a = 34;
+console.log(`this is my ${a}`)
+
+
+
+
+
+
